f51c79930db1ff83b432d1828160d83b908da45e
Bilal Ekrem Harmansa <bilalekremharmansa@gmail.com>
2020-03-29 14:18:51 +0300

    logger initialized -- log4j2

--- src/test/java/com/bilalekrem/healthcheck/model/TestHealthBuilder.java
+++ src/test/java/com/bilalekrem/healthcheck/model/TestHealthBuilder.java
@@ -1,13 +1,18 @@
 package com.bilalekrem.healthcheck.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestHealthBuilder {
 
+    private static Logger logger = LogManager.getLogger();
+
     @DisplayName("Test creation of Health")
     @Test
     void testCreateHealthWithBuilder() {
+        logger.info("Starting health creation test");
 
         Health health = Health.builder()
                 .status(HealthStatus.HEALTHY)
